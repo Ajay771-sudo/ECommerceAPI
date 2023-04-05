b3b5b14878b10513d42c454c5ad3047b50e229cb
eygdnet22 <eygdnet22@EYDNET37>
2023-04-05 18:29:45 +0530

    added an pi for ctegories

--- ECommerceAPI/DataAccess/IDataAccess.cs
+++ ECommerceAPI/DataAccess/IDataAccess.cs
@@ -5,5 +5,8 @@ namespace ECommerceAPI.DataAccess
     public interface IDataAccess
     {
         List<ProductCategory> GetProductCategories();
+        ProductCategory GetProductCategory(int id);
+        Offer GetOffer(int id);
+        List<Product> GetProducts(string category, string subcategory, int count);
     }
 }
